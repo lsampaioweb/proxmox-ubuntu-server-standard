c942b8862fe053f7393f3e42113ba6b1b3418cd4
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-01 00:04:41 -0300

    Added the terraform specific files for the stagging and production folder.

--- terraform/stagging/main.tf
+++ terraform/stagging/main.tf
@@ -1,6 +1,8 @@
 module "proxmox-ubuntu-22-04" {
   source = "../modules/proxmox-ubuntu-22-04"
 
-  clone = "ubuntu-22-04-server-raw"
-  name  = "ubuntu-22-04-server-raw-vm-stagging"
+  clone       = "ubuntu-22-04-server-standard"
+  name        = "ubuntu-22-04-server-standard-vm-stagging"
+  description = "Ubuntu 22.04 VM with the default applications and settings"
+  pool        = "Stagging"
 }

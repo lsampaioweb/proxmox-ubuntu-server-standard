caa80275ef9a2c0d515d1d8127668538beb06088
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-02 16:24:03 -0300

    Added the terraform and ansible scripts.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -2,7 +2,7 @@ module "proxmox-ubuntu-22-04" {
   source = "../modules/proxmox-ubuntu-22-04"
 
   clone       = "ubuntu-22-04-server-standard"
-  name        = "ubuntu-22-04-server-standard-vm-stagging"
+  name        = "ubuntu-22-04-server-standard-vm-production"
   description = "Ubuntu 22.04 VM with the default applications and settings"
-  pool        = "Production"
+  pool        = ""
 }
